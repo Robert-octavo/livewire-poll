5e54c3e689e60391eea0fb6c36ec2cbda4d15fc6
Robert-octavo <robert.ortega.octavo@gmail.com>
2024-03-18 16:47:07 -0500

    Add title and options functionality to CreatePoll component

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -6,8 +6,21 @@ use Livewire\Component;
 
 class CreatePoll extends Component
 {
+    public $title;
+    public $options = [''];
+
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
+    }
+
+    public function mount()
+    {
+        $this->options = [''];
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,4 +1,22 @@
 <div>
     {{-- Do your work, then step back. --}}
-    Hello, From Create Poll
+    <form action="">
+        <label for="">Poll Title</label>
+        <input type="text" wire:model="title" class="border-gray-300">
+
+        Current title: {{ $title }}<br>
+
+        <div class=" mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div class="mt-4">
+            @foreach ($options as $index => $option)
+                <label for="">Option {{ $index + 1 }}</label>
+                <input type="text" wire:model="options.{{ $index }}">
+
+                <button wire:click.prevent="removeOption({{ $index }})">-</button>
+            @endforeach
+        </div>
+    </form>
 </div>
