5104a0001d557dbc51fc0d29d9cfc85b893dace1
Robert-octavo <robert.ortega.octavo@gmail.com>
2024-03-19 16:15:35 -0500

    Add error messages for title and options in create-poll view

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -4,6 +4,10 @@
         <label for="">Poll Title</label>
         <input type="text" wire:model="title" class="border-gray-300">
 
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         {{-- Current title: {{ $title }}<br> --}}
 
         <div class=" mb-4 mt-4">
@@ -15,8 +19,12 @@
                 <label for="">Option {{ $index + 1 }}</label>
                 <div class="flex gap-2">
                     <input type="text" wire:model="options.{{ $index }}">
+
                     <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                 </div>
+                @error('options.' . $index)
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             @endforeach
         </div>
 
