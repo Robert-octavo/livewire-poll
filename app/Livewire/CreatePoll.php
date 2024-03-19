68732fa4963eade3c8306d569d99f3423448f934
Robert-octavo <robert.ortega.octavo@gmail.com>
2024-03-19 09:38:04 -0500

    Add createPoll() method to CreatePoll component

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Poll;
 use Livewire\Component;
 
 class CreatePoll extends Component
@@ -23,6 +24,36 @@ class CreatePoll extends Component
     {
         unset($this->options[$index]); // remove the option from the array
         $this->options = array_values($this->options); // reset array keys
+    }
+
+    public function createPoll()
+    {
+        $this->validate([
+            'title' => 'required',
+            'options.*' => 'required'
+        ]);
+
+        // Create the poll
+
+        // $poll = Poll::create([
+        //     'title' => $this->title
+        // ]);
+
+        // foreach ($this->options as $option) {
+        //     $poll->options()->create([
+        //         'name' => $option
+        //     ]);
+        // }
+
+        Poll::create([
+            'title' => $this->title
+        ])->options()->createMany(
+            collect($this->options) // convert the options to a collection
+                ->map(fn ($option) => ['name' => $option]) // map each option to an array
+                ->all() // convert the collection back to an array
+        );
+
+        $this->reset(['title', 'options']); // reset the form
     }
 
     public function mount()
